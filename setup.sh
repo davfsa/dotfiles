7d5c7740de1f280c33a77ae3453ac872d3c01c07
davfsa <davfsa@gmail.com>
2025-03-02 21:48:11 +0100

    Add home level dotfiles and script to symlink them
    This includes:
    - ideavimrc
    - p10k
    - zshrc

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Set bash "strict mode"
+set -euo pipefail
+IFS=$'\n\t'
+
+BASE=$(dirname "$0")
+
+link ()
+{
+    local file="$BASE/$1"
+
+    if [ "$#" -eq 1 ]; then
+        # If the destination is empty, then we will link it
+        # to ~/.original_filename
+        dest="$HOME/.$(basename "$file")"
+    else
+        dest="$2"
+    fi
+
+    if [ -f "$dest" ]; then
+        echo "[W] Making backup for ${dest}"
+        mv "$dest" "${dest}.back"
+    fi
+
+    echo "[I] Creating symlink $dest -> $file"
+    ln -nfs "$file" "$dest"
+}
+
+
+# Syslink the appropriate .config files
+link "zshrc"
+link "p10k.zsh"
+
+link "ideavimrc"
+
+link "tmux/tmux.conf"
